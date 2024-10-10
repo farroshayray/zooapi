40576b0877d534a044c6fa01d245c32bcc526a03
farroshayray <farros.hr@gmail.com>
2024-10-10 16:42:46 +0700

    add new coverage test

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,19 +1,17 @@
 import unittest
 from flask import Flask
-from main import app  # Asumsikan bahwa file utama Anda bernama main.py
+from main import app
 
 class HomeRouteTestCase(unittest.TestCase):
     def setUp(self):
-        # Set up the test client untuk Flask
         self.app = app.test_client()
         self.app.testing = True
 
     def test_home_route(self):
-        # Simulasi request GET ke home route
         response = self.app.get('/')
         
         assert response.status_code == 200
         assert b'Welcome Animal Lovers!' in response.data
 
-if __name__ == '__main__':
-    unittest.main()
+# if __name__ == '__main__':
+#     unittest.main()
